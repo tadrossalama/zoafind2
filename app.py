d0f8d7cb945ee4cd122beb7304ffc79244edf878
Tadros Salama <salamatadros@gmail.com>
2022-11-26 23:13:11 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastai.learner import export
+import streamlit as st
+import pickle
+from PIL import Image
+from fastai.vision.all import *
+from pathlib import Path
+import wikipedia
+from getinfo import *
+from pygbif import maps
+
+
+
+path = Path()
+learn_inf = load_learner(path/'learn3.pkl')
+
+
+st.title('Zoa Find :mag_right:')
+upload = st.file_uploader('Upload picture of coral here')
+if upload is not None:
+
+    image = PILImage.create(upload)
+    st.image(image, caption='Uploaded Image.')
+    pred,pred_idx,probs = learn_inf.predict(image)
+
+    st.title(f'I am {probs[pred_idx] * 100:.02f}% sure this is a {pred} coral.')
+    coral = Coral()
+    st.table(coral.get_species_name(pred))
+
+    st.write(wikipedia.summary(f'{pred} coral'))
+
+
