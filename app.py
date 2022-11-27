3101d55e4d0271f74701e00ea412b909ed045d1d
Tadros Salama <salamatadros@gmail.com>
2022-11-26 23:28:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ from pathlib import Path
 import wikipedia
 from getinfo import *
 from pygbif import maps
+from pygbif import occurrences as occ
+from pygbif import registry
+from pygbif import species
+import pandas as pd
+
+
+class Coral:
+    def __init__(self):
+        pass
+
+    def get_species_name(self, name):
+        """
+        Get the species name from the taxon key
+        """
+        name = species.name_suggest(q = name, limit=1)
+        df = pd.DataFrame(name)
+        return df[['kingdom', 'phylum', 'class', 'order', 'family', 'scientificName', 'canonicalName']]
+
 
 
 
